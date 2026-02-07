cdd96d7a8b2b82d4943610eb56af92348649b442
codecreatewrite <efetobookpubigho@gmail.com>
2026-02-07 19:10:22 +0100

    StudyCore all set & fully ready for verification

--- app/services/google_auth.py
+++ app/services/google_auth.py
@@ -10,7 +10,6 @@ SCOPES = [
     'openid',
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/calendar.events'
 ]
 
 class GoogleAuthService:
